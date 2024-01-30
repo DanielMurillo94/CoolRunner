9234155971dd87bb11247346ccf43d478a7d4c31
Daniel Murillo <danmurilloac@gmail.com>
2024-01-29 22:12:28 -0600

    Variable height of jump

--- Player.cpp
+++ Player.cpp
@@ -8,8 +8,9 @@
 Player::Player(Vector2 _position, float _jumpHeight, float _jumpTime, int _groundLevel) : jumpHeight(_jumpHeight),
                                                                                           jumpTime(_jumpTime),
                                                                                           jumpForce((-(2.0f * _jumpHeight) / _jumpTime)),
-                                                                                          gravity((2.0f * _jumpHeight / (_jumpTime * _jumpTime))),
-                                                                                          //   originalHeight(_position.y),
+                                                                                          highJumpGravity((2.0f * _jumpHeight / (_jumpTime * _jumpTime))),
+                                                                                          lowJumpGravity(highJumpGravity * 3),
+                                                                                          currentGravity(highJumpGravity),
                                                                                           isInGround(true)
 {
     width = 100.f;
@@ -19,48 +20,58 @@ Player::Player(Vector2 _position, float _jumpHeight, float _jumpTime, int _groun
     velocity = Vector2{0.f, 0.f};
     area = Rectangle{};
     std::cout << "Constructing a player";
+    Reset();
 }
 
 void Player::Jump()
 {
     if (isInGround)
     {
-        printf("Jumping");
         velocity.y = jumpForce;
         isInGround = false;
     }
 }
 
+void Player::FallFaster()
+{
+    if (!isInGround)
+    {
+        currentGravity = lowJumpGravity;
+    }
+}
+
+void Player::Reset()
+{
+    position.y = originalHeight;
+    isInGround = true;
+    velocity.y = 0;
+    currentGravity = highJumpGravity;
+    area.y = originalHeight;
+}
+
 void Player::Update(float deltaTime)
 {
-    if (position.y >= originalHeight)
-    {
-        position.y = originalHeight;
-        isInGround = true;
-        velocity.y = 0;
-    }
 
     if (IsKeyPressed(KEY_SPACE))
     {
         Jump();
     }
+    if (IsKeyReleased(KEY_SPACE))
+    {
+        FallFaster();
+    }
 
     if (!isInGround)
     {
-        velocity.y += (gravity * deltaTime);
+        velocity.y += (currentGravity * deltaTime);
     }
 
-    if (position.y < highestHeight)
-    {
-        highestHeight = position.y;
-        std::string heightText{"Height: "};
-        printf(heightText.append(std::to_string(highestHeight)).c_str());
-        std::cout << "Jump to Y pixel: " << jumpHeight << "  GroundHeight: " << originalHeight << "\n";
-        std::cout << "JumpForce: " << jumpForce << "  jumpTime: " << jumpTime << "\n";
-        std::cout << "Gravity: " << gravity << "\n";
-        std::cout << "Jump Height: " << originalHeight - highestHeight << "\n";
-    }
     BaseObject::Update(deltaTime);
+
+    if (position.y >= originalHeight && !isInGround)
+    {
+        Reset();
+    }
 
     DrawRectangle(area.x, area.y, area.width, area.height, RED);
     for (int i = originalHeight; i > 0; i -= 100)
@@ -68,3 +79,14 @@ void Player::Update(float deltaTime)
         DrawLine(0, i, 1000, i, BLACK);
     }
 }
+
+// if (position.y < highestHeight)
+// {
+//     highestHeight = position.y;
+//     std::string heightText{"Height: "};
+//     printf(heightText.append(std::to_string(highestHeight)).c_str());
+//     std::cout << "Jump to Y pixel: " << jumpHeight << "  GroundHeight: " << originalHeight << "\n";
+//     std::cout << "JumpForce: " << jumpForce << "  jumpTime: " << jumpTime << "\n";
+//     std::cout << "Gravity: " << currentGravity << "\n";
+//     std::cout << "Jump Height: " << originalHeight - highestHeight << "\n";
+// }
--- Player.h
+++ Player.h
@@ -6,14 +6,21 @@ class Player : BaseObject
 private:
     float jumpHeight; // Jump height in pixels
     float jumpTime;   // Time it'll take for the player to reach the max height of the jump
-    float gravity{};
     float jumpForce{};
+
+    const float highJumpGravity{};
+    const float lowJumpGravity{};
+    float currentGravity{};
+
     bool isInGround{true};
     float originalHeight{};
     float highestHeight{1500};
 
+    void FallFaster();
+    void Jump();
+    void Reset();
+
 public:
     Player(Vector2 _position, float _jumpHeight, float _jumpTime, int _groundLevel);
-    void Jump();
     virtual void Update(float deltaTime);
 };
